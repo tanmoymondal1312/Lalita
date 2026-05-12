59e0b87e6c37beb21edd7f9bb2e4eba8a9c3a159
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-05-12 23:42:49 +0600

    Round 3: Show digit count of factorial result

--- factorial.py
+++ factorial.py
@@ -1,16 +1,17 @@
 num = int(input("Enter a number to find Factorial: "))
 
-factorial = 1
-
 print(f"\n--- Factorial of {num} ---")
 
 if num < 0:
     print("Factorial is not defined for negative numbers")
 elif num == 0:
-    print("Factorial of 0 is 1")
+    print("Factorial of 0 = 1")
 else:
+    factorial = 1
     for i in range(1, num + 1):
         factorial = factorial * i
+
     formula = " x ".join(str(i) for i in range(1, num + 1))
     print(f"Formula   : {formula}")
-    print(f"Factorial of {num} = {factorial}")
+    print(f"Answer    : {num}! = {factorial}")
+    print(f"Digits    : {len(str(factorial))} digit number")
