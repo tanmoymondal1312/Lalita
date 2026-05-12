51f42f93b3e1b32d019dfae4a3ab94da41af8ec8
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-05-12 23:38:49 +0600

    Round 1: Optimize prime check using square root, add header

--- prime.py
+++ prime.py
@@ -1,10 +1,12 @@
 num = int(input("Enter a number to check Prime: "))
 
+print(f"\n--- Prime Check for {num} ---")
+
 if num <= 1:
     print(f"{num} Is Not a Prime Number")
 else:
     is_prime = True
-    for i in range(2, num):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             is_prime = False
             break
