4fa30927c80eb9ddfc029457aa5543e3136f934d
Tanmoy Mondal <tanmoymondal1315@gmail.com>
2026-05-12 22:39:53 +0600

    Add Factorial algorithm

---
+++ factorial.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number to find Factorial: "))
+
+factorial = 1
+
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+elif num == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial = factorial * i
+    print(f"Factorial of {num} is {factorial}")
